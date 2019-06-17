05861a20273a2b583038c09f84426cc2fec7b47c
K H <chandrakala.k-h@capgemini.com>
2019-06-17 15:07:29 +0530

    frst terraform code

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  access_key = "AKIAIFLFLZBPHE5C3CYQ"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "remote" {
+  # AMI ID for Amazon Linux AMI 2016.03.0 (HVM)
+  ami           = "ami-098bb5d92c8886ca1"
+  instance_type = "t2.micro"
+  key_name      = "devops"
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install epel-release",
+      "sudo yum -y install nginx",
+      "sudo service nginx start"
+    ]
+    connection {
+      host = "${aws_instance.remote.public_ip}"
+      type = "ssh"
+      user = "ec2-user"
+      private_key = "${file("./devops.pem")}"
+    }
+  }
+  }
